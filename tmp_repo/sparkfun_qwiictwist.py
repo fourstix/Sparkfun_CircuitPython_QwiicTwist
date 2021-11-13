08b44ff9a22f5cdcec7fda84ccc39287681dd9e2
Gaston Williams <fourstix@gmail.com>
2021-11-13 10:08:29 -0500

    Update license and setup files

---
+++ tmp_repo/sparkfun_qwiictwist.py
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2021 Gaston Williams
+#
+# SPDX-License-Identifier: MIT
+"""
+`sparkfun_qwiictwist`
+================================================================================
+
+CircuitPython library for the Sparkfun Qwiic Twist RGB Rotary Encoder
+
+
+* Author(s): Gaston Williams
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
+# imports__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/fourstix/Sparkfun_CircuitPython_QwiicTwist.git"
